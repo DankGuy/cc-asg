b6b4120a39b3667f3fd863454798e0c632c20ea8
DankGuy <68477088+DankGuy@users.noreply.github.com>
2023-09-21 18:04:06 +0800

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "cc-assg.c9pulvddhfux.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "assg"
+custombucket = "chongxinyi-bucket"
+customregion = "us-east-1"
